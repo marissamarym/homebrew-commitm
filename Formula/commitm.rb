5e83205d763ae1dd1782663a4b1d5e2506d06094
Marissa Montgomery <m@m15y.com>
2024-02-26 11:28:09 -0500

    Release fix

--- Formula/commitm.rb
+++ Formula/commitm.rb
@@ -1,7 +1,7 @@
 class Commitm < Formula
   desc "Use the commitm command to generate commit messages, modify them, and commit!"
   homepage "https://github.com/marissamarym/commitm"
-  url "https://github.com/marissamarym/commitm/releases/download/v1.0.5/commitm-v1.0.7.tar.gz"
+  url "https://github.com/marissamarym/commitm/releases/download/v1.0.7/commitm-v1.0.7.tar.gz"
   sha256 "205f20a1d34372bec08a3ac7b5cddcd5f25bd6af82dd4df178e9a73fb72816fe"
   license "MIT"
 
